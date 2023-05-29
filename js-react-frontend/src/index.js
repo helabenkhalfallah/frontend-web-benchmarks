5f6d0fbdb33ec314da57050db74325596ad763c3
helabenkhalfallah <helabenkhalfallah@hotmail.fr>
2023-05-29 17:43:28 +0200

    [FEATURE]: import required core-js module

--- js-react-frontend/src/index.js
+++ js-react-frontend/src/index.js
@@ -1,5 +1,9 @@
+import 'core-js/es/array';
+import 'core-js/es/object';
+import 'core-js/es/promise';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import 'regenerator-runtime/runtime';
 import Router from './routes/Router';
 
 const container = document.getElementById('js-react-frontend');
